5805ec8af4938e6f58f9fbe8632877bf906ce763
meetabug <1272526724@qq.com>
2019-08-29 17:20:55 +0800

    implement my posts

---
+++ app/controllers/account/posts_controller.rb
@@ -0,0 +1,12 @@
+class Account::PostsController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def index
+    @posts = current_user.posts.recent.paginate(:page => params[:page], :per_page => 5)
+  end
+
+  def edit
+
+  end
+end
